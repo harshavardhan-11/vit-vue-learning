c0314b9af82c30bddc964978cde3b57cf4d55fe4
Harshavardhan Singam <harshavardahn singam>
2024-08-29 14:29:44 +0530

    Store input data to localStorage with the helo of composables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import InputView from './InputView.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <InputView></InputView>
   </main>
 </template>
---
+++ src/views/InputView.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { useStorage } from "@/composables/useStorage";
+  let name = useStorage('name', 'Harsha');
+  let age = useStorage('age', 27);
+</script>
+
+<template>
+  <main>
+    <p>
+      What is your name? <input type="text" v-model="name">
+      What is your age? <input type="text" v-model="age">
+    </p>
+  </main>
+</template>
